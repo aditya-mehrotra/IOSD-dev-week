2cd305e0dc2d2572e460a601595043a20001729f
aditya-mehrotra <73335226+aditya-mehrotra@users.noreply.github.com>
2021-01-21 19:46:19 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,8 @@ class MyApp extends StatelessWidget {
     allPeople = [Person("Maanas", "Why so serious?")];
     allPeople.add(Person("Shubh", "Alright, Alright, Alright!"));
 //    allPeople.add(Person("Shubh", "Why so serious?"));
+   allPeople.add(Person("Aditya", "Hello world"));
+
 
     return MaterialApp(
       home: Scaffold(
